410b0789f9974cb17101a8abb9ff60900d6a849e
Sebstian Golebiowski <kontakt@sebastian-webdev.pl>
2019-04-25 07:16:54 +0200

    Updates

---
+++ precache-manifest.56b04e1b8de2f2154b35f583d8d3d70f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc85d62421fbc46ab3d5",
+    "url": "/task-app-front/static/css/main.3e007339.chunk.css"
+  },
+  {
+    "revision": "fc85d62421fbc46ab3d5",
+    "url": "/task-app-front/static/js/main.90e0b064.chunk.js"
+  },
+  {
+    "revision": "c9f5cbc6f499be0c0fad",
+    "url": "/task-app-front/static/js/runtime~main.5f1e04b2.js"
+  },
+  {
+    "revision": "7dfa322c6086ceab6a1e",
+    "url": "/task-app-front/static/js/2.cc62eef6.chunk.js"
+  },
+  {
+    "revision": "f83b87617740f6f03981d6a1c1a52599",
+    "url": "/task-app-front/index.html"
+  }
+];
